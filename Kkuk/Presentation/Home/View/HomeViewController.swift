2738a6bd567545a5931d3c75df9cdda0f638a512
se-ryeong <seryz029@gmail.com>
2023-10-18 15:42:50 +0900

    design: 배경색설정,오늘의 추천문구 추가(#18)  ↑

--- Kkuk/Presentation/Home/View/HomeViewController.swift
+++ Kkuk/Presentation/Home/View/HomeViewController.swift
@@ -11,20 +11,49 @@ import SnapKit
 
 class HomeViewController: BaseUIViewController {
     
-    lazy var label: UILabel = UILabel().and {
-        $0.text = "익스텐션 테스트"
-        $0.font = .largeTitle
-        $0.textColor = .main
+    private var backgroundView: UIView = UIView().and {
+        $0.backgroundColor = .main
+    }
+    
+    private var homeLabel: UILabel = UILabel().and {
+        $0.text = "이 콘텐츠는 어떠세요?"
+        $0.font = .subtitle2
+        $0.textColor = .background
+    }
+    
+    private var recommendLabel: UILabel = UILabel().and {
+        $0.text = "오늘의 추천"
+        $0.font = .title1
+        $0.textColor = .background
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
     }
     
     override func setUI() {
-        view.addSubview(label)
+        view.addSubview(backgroundView)
+        view.addSubview(homeLabel)
+        view.addSubview(recommendLabel)
     }
     
     override func setLayout() {
-        label.snp.makeConstraints { constraint in
-            constraint.centerX.equalToSuperview()
-            constraint.centerY.equalToSuperview()
+        
+        backgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.height.equalTo(450)
+            $0.width.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        homeLabel.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            $0.leading.equalTo(20)
+        }
+        
+        recommendLabel.snp.makeConstraints {
+            $0.top.equalTo(homeLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(20)
         }
     }
     
