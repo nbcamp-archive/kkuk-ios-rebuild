35f8e0c7a27c45be76f527ee898b0d20d68315c4
jooYeon <conn0204@naver.com>
2023-10-18 18:24:25 +0900

    design: 검색창 추가(#19)

--- Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
+++ Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
@@ -9,12 +9,41 @@ import UIKit
 
 class SearchContentViewController: BaseUIViewController {
     
-    override func setUI() {}
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "검색어를 입력하세요"
+        searchBar.delegate = self
+        
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.backgroundColor = .clear
+            textField.layer.borderWidth = 1.0
+            textField.layer.borderColor = UIColor.black.cgColor
+            textField.layer.cornerRadius = 12.0
+          }
+        
+        return searchBar
+    }()
     
-    override func setLayout() {}
+    override func setUI() {
+        view.addSubviews([searchBar])
+    }
+    
+    override func setLayout() {
+        setSearchBarLayout()
+    }
     
     override func setDelegate() {}
     
     override func addTarget() {}
+    
+    func setSearchBarLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
+
+extension SearchContentViewController: UISearchBarDelegate {
     
 }
