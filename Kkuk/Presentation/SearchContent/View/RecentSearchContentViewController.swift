2887e85b1affa45018ccead10622b270a1c06395
jooYeon <conn0204@naver.com>
2023-10-19 14:38:42 +0900

    design: 최근 검색어 collectionView 추가(#19)

--- Kkuk/Presentation/SearchContent/View/RecentSearchContentViewController.swift
+++ Kkuk/Presentation/SearchContent/View/RecentSearchContentViewController.swift
@@ -18,13 +18,23 @@ class RecentSearchContentViewController: BaseUIViewController {
         return label
     }()
 
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(BaseUICollectionViewCell.self, forCellWithReuseIdentifier: "BaseUICollectionViewCell")
+        return collectionView
+    }()
+
+    
     override func setUI() {
-        view.addSubviews([recentSearchesLabel])
+        view.addSubviews([recentSearchesLabel, collectionView])
 
     }
     
     override func setLayout() {
         setRecentSearchesLabel()
+        setCollectionViewLayout()
     }
     
     func setRecentSearchesLabel() {
@@ -33,5 +43,27 @@ class RecentSearchContentViewController: BaseUIViewController {
             make.leading.trailing.equalToSuperview().inset(20)
         }
     }
+    
+    func setCollectionViewLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(recentSearchesLabel.snp.bottom).offset(32)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+}
 
+extension RecentSearchContentViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BaseUICollectionViewCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
 }
