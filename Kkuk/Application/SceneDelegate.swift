ddf6231a1f150e1a07ca52b3de349952a5ab65be
yujinkim1 <yujin7621@me.com>
2023-10-22 23:17:20 +0900

    style: IQKeyboardManagerSwift 사용 방식 변경
    - 해당 라이브러리가 필요한 특정 화면에서만 적용되도록 수정

--- Kkuk/Application/SceneDelegate.swift
+++ Kkuk/Application/SceneDelegate.swift
@@ -14,10 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.windowScene = windowScene
-        window?.rootViewController = AppTabBarController()
         
         window?.makeKeyAndVisible()
+        window?.rootViewController = AppTabBarController()
     }
     
 }
--- Kkuk/Global/Base/BaseUIViewController.swift
+++ Kkuk/Global/Base/BaseUIViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Yujin Kim on 2023-10-15.
 //
 
+import IQKeyboardManagerSwift
 import UIKit
 
 class BaseUIViewController: UIViewController {
@@ -21,6 +22,8 @@ class BaseUIViewController: UIViewController {
     }
     // `BaseUIViewController`를 상속받은 객체는 해당 함수를 사용할 수 있습니다.
     //
+    /// 네비게이션 바 구성을 위한 사용자 정의 함수 입니다.
+    func setNavigationBar() {}
     /// UI 구성을 위한 사용자 정의 함수입니다.
     func setUI() {}
     /// 화면 레이아웃을 설정하기 위한 사용자 정의 함수입니다.
@@ -29,5 +32,17 @@ class BaseUIViewController: UIViewController {
     func setDelegate() {}
     /// 타겟-액션 패턴을 지정하기 위한 사용자 정의 함수입니다.
     func addTarget() {}
+    
+}
+
+// MARK: - IQKeyboardManagerSwift 활성화 메서드
+
+extension BaseUIViewController {
+    
+    func setIQKeyboardManagerEnable(_ enabled: Bool) {
+        IQKeyboardManager.shared.enable = enabled
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = enabled
+        IQKeyboardManager.shared.enableAutoToolbar = !enabled
+    }
     
 }
