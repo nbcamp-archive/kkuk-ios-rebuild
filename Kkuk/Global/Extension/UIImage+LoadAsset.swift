fb31fd4257c32d641614f4281745acb3b279513a
yujinkim1 <yujin7621@me.com>
2023-10-24 11:36:45 +0900

    feat: 에셋 이미지 불러오기 메서드 추가(#32)
    - 에셋 이미지 리터럴을 적용하기 위해 메서드 추가 제공
    - 만약 해당 에셋 이미지를 찾을 수 없으면 Empty 이미지 반환

---
+++ Kkuk/Global/Extension/UIImage+LoadAsset.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage+LoadAsset.swift
+//  Kkuk
+//
+//  Created by Yujin Kim on 2023-10-24.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func loadAsset(named name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+}
