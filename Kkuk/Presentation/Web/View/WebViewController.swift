2056c95b6e7102a93f5ee16b1d0c68d8ca75a988
yujinkim1 <yujin7621@me.com>
2023-10-25 20:40:22 +0900

    chore: 웹 화면 컨트롤러 추가(#46)

---
+++ Kkuk/Presentation/Web/View/WebViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WebViewController.swift
+//  Kkuk
+//
+//  Created by Yujin Kim on 2023-10-25.
+//
+
+import UIKit
+
+class WebViewController: BaseUIViewController {
+    
+    override func setUI() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+    
+    override func setDelegate() {
+        
+    }
+    
+    override func addTarget() {
+        
+    }
+    
+}
