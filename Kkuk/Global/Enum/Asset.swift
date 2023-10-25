b2809b21f3b5f370ccdc7f3780b81ba44bcd855d
yujinkim1 <yujin7621@me.com>
2023-10-25 21:00:11 +0900

    feat: 웹 화면 툴바 이미지 에셋 추가(#46)

--- Kkuk/Global/Enum/Asset.swift
+++ Kkuk/Global/Enum/Asset.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 enum Asset {
+    
     // MARK: - TabBarItem
+    
     static var home: UIImage { .loadAsset(named: "lucide_home") }
     static var category: UIImage { .loadAsset(named: "lucide_category") }
     static var search: UIImage { .loadAsset(named: "lucide_search") }
     static var setting: UIImage { .loadAsset(named: "lucide_setting") }
+    
+    // MARK: - WebView
+    
+    static var backward: UIImage { .loadAsset(named: "lucide_left_arrow") }
+    static var foward: UIImage { .loadAsset(named: "lucide_right_arrow") }
+    static var safari: UIImage { .loadAsset(named: "lucide_safari") }
+    static var share: UIImage { .loadAsset(named: "lucide_share") }
     
 }
