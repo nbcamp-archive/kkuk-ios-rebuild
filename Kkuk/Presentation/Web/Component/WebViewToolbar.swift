2dc5f9cc7a987c0cfb64dfc3b0ea3a75d6e4aa14
yujinkim1 <yujin7621@me.com>
2023-10-25 20:44:12 +0900

    chore: 웹 화면 툴바 추가(#46)

---
+++ Kkuk/Presentation/Web/Component/WebViewToolbar.swift
@@ -0,0 +1,21 @@
+//
+//  WebViewToolbar.swift
+//  Kkuk
+//
+//  Created by Yujin Kim on 2023-10-25.
+//
+
+import UIKit
+
+class WebViewToolbar: UIToolbar {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
