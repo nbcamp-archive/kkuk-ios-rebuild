31000d74b54de620312be5684d3015d2e8415758
yujinkim1 <yujin7621@me.com>
2023-10-27 17:00:09 +0900

    feat: 툴바 아이템 지정(#46)

--- Kkuk/Presentation/Web/Component/WebViewToolbar.swift
+++ Kkuk/Presentation/Web/Component/WebViewToolbar.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class WebViewToolbar: UIToolbar {
     
+    lazy var backwardButton = UIBarButtonItem(image: Asset.backward.withRenderingMode(.alwaysTemplate), style: .plain, target: nil, action: nil)
+    
+    lazy var forwardButton = UIBarButtonItem(image: Asset.forward.withRenderingMode(.alwaysTemplate), style: .plain, target: nil, action: nil)
+    
+    lazy var shareButton = UIBarButtonItem(image: Asset.share.withRenderingMode(.alwaysTemplate), style: .plain, target: nil, action: nil)
+    
+    lazy var safariButton = UIBarButtonItem(image: Asset.safari.withRenderingMode(.alwaysTemplate), style: .plain, target: nil, action: nil)
+    
+    lazy var flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    
+    lazy var barButtonItems = [backwardButton, flexibleSpace, forwardButton, flexibleSpace, shareButton, flexibleSpace, safariButton]
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        isTranslucent = false
+        barTintColor = .background
+        
+        barButtonItems.forEach {
+            $0.tintColor = .selected
+        }
+        
+        setItems(barButtonItems, animated: false)
     }
     
     @available(*, unavailable)
