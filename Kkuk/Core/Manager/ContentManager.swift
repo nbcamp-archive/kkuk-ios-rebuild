45b1d1d0339fae95d93b76fd0ef90891f01df0ae
jooYeon <conn0204@naver.com>
2023-10-28 16:54:04 +0900

    style: 인덴트 수정(#64)

--- Kkuk/Core/Manager/ContentManager.swift
+++ Kkuk/Core/Manager/ContentManager.swift
@@ -11,29 +11,29 @@ import RealmSwift
 class ContentManager {
     func getLocationOfDefaultRealm() {
             print("Realm is located at:", try! Realm().configuration.fileURL!)
-        }
+    }
 
-        func create(content: Content) {
-            do {
-                let realm = try Realm()
-                try realm.write {
-                    realm.add(content)
-                }
-            } catch {
-                print("Failed create ContentObject: \(error)")
+    func create(content: Content) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(content)
             }
+        } catch {
+            print("Failed create ContentObject: \(error)")
         }
-
-        func read() -> [Content] {
-            do {
-                let realm = try Realm()
-                let contents = realm.objects(Content.self).sorted(byKeyPath: "createDate", ascending: false)
-                return Array(contents)
-            } catch {
-                print("Failed read ContentObject: \(error)")
-            }
-            return []
+    }
+    
+    func read() -> [Content] {
+        do {
+            let realm = try Realm()
+            let contents = realm.objects(Content.self).sorted(byKeyPath: "createDate", ascending: false)
+            return Array(contents)
+        } catch {
+            print("Failed read ContentObject: \(error)")
         }
+        return []
+    }
     
     func read(at searchText: String) -> [Content] {
         do {
