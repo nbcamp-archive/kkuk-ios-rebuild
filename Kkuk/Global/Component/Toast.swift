6e2b04fc548017ff9ceb4ae181aeda07f118663e
yujinkim1 <yujin7621@me.com>
2023-10-29 00:04:24 +0900

    feat: 토스트 컴포넌트 추가(#60)
    - 사용자에게 이벤트 처리 알림을 보여주기 위해 사용할 토스트 기능을 미리 추가

---
+++ Kkuk/Global/Component/Toast.swift
@@ -0,0 +1,43 @@
+//
+//  Toast.swift
+//  Kkuk
+//
+//  Created by Yujin Kim on 2023-10-28.
+//
+
+import UIKit
+
+class Toast {
+
+    static func showToast(_ viewController: UIViewController, message: String) {
+        let xPosition = viewController.view.frame.size.width
+        let yPosition = viewController.view.safeAreaInsets.top
+
+        let frame = CGRect(x: xPosition / 2 - 75, y: yPosition, width: 179, height: 44)
+
+        let toastLabel = UILabel(frame: frame)
+        toastLabel.backgroundColor = .black.withAlphaComponent(0.6)
+        toastLabel.alpha = CGFloat(1)
+        toastLabel.font = .subtitle3
+        toastLabel.layer.cornerRadius = CGFloat(16)
+        toastLabel.text = message
+        toastLabel.textColor = .white
+        toastLabel.textAlignment = .center
+        toastLabel.clipsToBounds = true
+
+        viewController.view.addSubview(toastLabel)
+
+        UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 1.0
+            toastLabel.frame.origin.y += 50
+        }, completion: { _ in
+            UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseIn, animations: {
+                toastLabel.alpha = 0.0
+                toastLabel.frame.origin.y -= 50
+            }, completion: { _ in
+                toastLabel.removeFromSuperview()
+            })
+        })
+    }
+
+}
