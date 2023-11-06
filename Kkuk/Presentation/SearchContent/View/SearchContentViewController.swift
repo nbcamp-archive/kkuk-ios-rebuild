4fd85b94f95aa588eb56ea47e04eff509e9056fe
jooYeon <conn0204@naver.com>
2023-11-06 13:06:15 +0900

    refactor: enum 파일 경로 이동(#80)

--- Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
+++ Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
@@ -7,25 +7,6 @@
 
 import UIKit
 
-enum SegmentMenu: Int {
-    case title
-    case memo
-
-    var name: String {
-        switch self {
-        case .title: return "사이트"
-        case .memo: return "메모"
-        }
-    }
-
-    var column: String {
-        switch self {
-        case .title: return "title"
-        case .memo: return "memo"
-        }
-    }
-}
-
 class SearchContentViewController: BaseUIViewController {
     
     var contentList: [Content] = []
