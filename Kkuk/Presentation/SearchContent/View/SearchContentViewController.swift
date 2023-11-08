c757125379a5aa90d97122e027115a136e33aa6a
jooYeon <conn0204@naver.com>
2023-11-08 18:48:46 +0900

    design: 다크모드에서 배경화면색과 동일하게 수정(#118)

--- Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
+++ Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
@@ -42,6 +42,7 @@ class SearchContentViewController: BaseUIViewController {
         tableView.delegate = self
         tableView.register(ContentTableViewCell.self, forCellReuseIdentifier: "ContentTableViewCell")
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.backgroundColor = .background
         return tableView
     }()
     
