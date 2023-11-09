97a2020e6bf05b8abad133d802db22dfa18a1696
yujinkim1 <yujin7621@me.com>
2023-11-10 00:50:26 +0900

    refactor: 콘텐츠 서비스 구조 개선
    - 일관된 형태로 Realm 트랜색션을 오픈할 수 있도록 공통 함수 정의

--- Kkuk/Domain/Helper/ContentHelper.swift
+++ Kkuk/Domain/Helper/ContentHelper.swift
@@ -11,9 +11,42 @@ import Foundation
 
 class ContentHelper {
     
+    private var database: Realm {
+        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.archive.nbcamp.Kkuk")
+        
+        let realmURL = container?.appendingPathComponent("default.realm")
+        
+        let config = Realm.Configuration(fileURL: realmURL, schemaVersion: 1)
+        
+        do {
+            return try Realm(configuration: config)
+        } catch {
+            fatalError("Error initializing Realm Service: \(error.localizedDescription)")
+        }
+    }
+    
+    private func databaseTransaction(_ action: () -> Void) {
+        do {
+            try database.write { action() }
+        } catch {
+            printError("Failed to perform database action: \(error.localizedDescription)")
+        }
+    }
+    
+    private func printError(_ message: String) {
+        print(message)
+    }
+    
+}
+
+// MARK: - Realm Action
+
+extension ContentHelper {
+    
     func getLocationOfDefaultRealm() {
         do {
             let realm = try Realm()
+            
             let filePath = realm.configuration.fileURL
             print("Realm is located at: ", filePath!)
         } catch {
@@ -21,76 +54,38 @@ class ContentHelper {
         }
     }
     
-    private var database: Realm {
-        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.archive.nbcamp.Kkuk")
-        let realmURL = container?.appendingPathComponent("default.realm")
-        let config = Realm.Configuration(fileURL: realmURL, schemaVersion: 1)
-        do {
-            return try Realm(configuration: config)
-        } catch {
-            fatalError("Error initializing Realm: \(error)")
-        }
-    }
-    
     func create(content: Content) {
-        do {
-            try database.write {
-                database.add(content)
-            }
-        } catch {
-            print("Failed create ContentObject: \(error)")
-        }
-    }
-    
-    func read() -> [Content] {
-        do {
-            let contents = database.objects(Content.self).sorted(byKeyPath: "createDate", ascending: false)
-            return Array(contents)
-        } catch {
-            print("Failed read ContentObject: \(error)")
-        }
-        return []
-    }
-    
-    func read(at column: SegmentMenu, with searchText: String) -> [Content] {       
-        do {
-            let query = NSPredicate(format: "\(column) CONTAINS[c] %@", searchText)
-            let result = database.objects(Content.self).filter(query).sorted(byKeyPath: "createDate", ascending: false)
-            return Array(result)
-        } catch {
-            print("Error adding user: \(error)")
-            return []
-        }
+        databaseTransaction { database.add(content) }
     }
     
     func update(content: Content, completion: @escaping (Content) -> Void) {
-        do {
-            try database.write {
-                completion(content)
-            }
-        } catch {
-            print("Failed to update ContentObject: \(error)")
-        }
+        databaseTransaction { completion(content) }
+    }
+    
+    func read() -> [Content] {
+        let contents = database.objects(Content.self).sorted(byKeyPath: "createDate", ascending: false)
+        
+        return Array(contents)
+    }
+    
+    func read(at column: SegmentMenu, with searchText: String) -> [Content] {
+        let query = NSPredicate(format: "\(column) CONTAINS[c] %@", searchText)
+        
+        let result = database.objects(Content.self).filter(query).sorted(byKeyPath: "createDate", ascending: false)
+        
+        return Array(result)
     }
     
     func readInCategory(at id: ObjectId) -> [Content] {
-        do {
-            let predicateQuery = NSPredicate(format: "category == %@", id)
-            let contents = database.objects(Content.self).sorted(byKeyPath: "createDate", ascending: false).filter(predicateQuery)
-            return Array(contents)
-        } catch {
-            print("Failed read ContentObject: \(error)")
-        }
-        return []
+        let predicateQuery = NSPredicate(format: "category == %@", id)
+        
+        let contents = database.objects(Content.self).sorted(byKeyPath: "createDate", ascending: false).filter(predicateQuery)
+        
+        return Array(contents)
     }
     
     func delete(_ content: Content) {
-        do {
-            try database.write {
-                database.delete(content)
-            }
-        } catch {
-            print("Failed read ContentObject: \(error)")
-        }
+        databaseTransaction { database.delete(content) }
     }
+    
 }
