69d3c72bae8dbf0615c7c17f5d87d23b21c77b66
jooYeon <conn0204@naver.com>
2023-11-10 11:21:03 +0900

    fix: 검색화면 콘텐츠 수정 후 팬모달 사라지기와 콘텐츠 리로드하도록 수정(#145)

--- Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
+++ Kkuk/Presentation/SearchContent/View/SearchContentViewController.swift
@@ -262,7 +262,9 @@ extension SearchContentViewController: ContentTableViewCellDelegate {
                      PanModalOption.Title.share,
                      PanModalOption.Title.cancel]
         let option = PanModalOption(screenType: .content, title: title)
-        let modalVC = PanModalTableViewController(option: option, content: content)
+        let modalVC = PanModalTableViewController(option: PanModalOption(screenType: .content, title: title), content: content, completion: {
+            self.reloadData()
+        })
         modalVC.modalPresentationStyle = .popover
         presentPanModal(modalVC)
     }
