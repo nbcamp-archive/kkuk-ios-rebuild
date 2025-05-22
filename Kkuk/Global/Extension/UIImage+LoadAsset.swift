0be680d5d4bae5a85e20bba11ceba9f87886aa5d
yujinkim1 <yujin7621@me.com>
2025-05-22 12:04:09 +0900

    chore: 메인 액터 애트리뷰트 적용
    Swift 6로 마이그레이션하기 위해 기존 코드에 임시로 사용

--- Kkuk/Global/Extension/UIImage+LoadAsset.swift
+++ Kkuk/Global/Extension/UIImage+LoadAsset.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
+// !!!: Command SwiftCompile failed with a nonzero exit code
+// CategoryHelper.swift 참고
+// UIImage는 non-sendable한 객체지만 @unchecked Sendable 시도는 Swift 6에선 허용되지 않음
 extension UIImage {
-    
+    @MainActor
     static func loadAsset(named name: String) -> UIImage {
         guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
             return UIImage()
