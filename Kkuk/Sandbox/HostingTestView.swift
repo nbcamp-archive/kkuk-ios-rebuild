c83812e98dc3f969106f2f287f1c2ba0f8c4249f
yujinkim1 <yujin7621@me.com>
2025-05-27 00:35:54 +0900

    test: HostingTestView.swift 파일 추가
    Sandbox 폴더에서 SwiftUI 뷰 호스팅 테스트(#1)

---
+++ Kkuk/Sandbox/HostingTestView.swift
@@ -0,0 +1,18 @@
+//
+//  HostingTestView.swift
+//  Kkuk
+//
+//  Created by Yujin Kim on 2025-05-27.
+//
+
+import SwiftUI
+
+struct HostingTestView: View {
+    var body: some View {
+        Text("\(self)")
+    }
+}
+
+#Preview {
+    HostingTestView()
+}
