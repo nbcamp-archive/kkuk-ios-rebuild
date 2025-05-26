fc51cb64093b3dbf249a42a0df17b0b10913f81b
yujinkim1 <yujin7621@me.com>
2025-05-27 01:24:08 +0900

    chore: KkukApp.swift 파일 추가(#2)
    새로운 진입점 구성

---
+++ Kkuk/App/KkukApp.swift
@@ -0,0 +1,17 @@
+//
+//  KkukApp.swift
+//  Kkuk
+//
+//  Created by Yujin Kim on 2025-05-27.
+//
+
+import SwiftUI
+
+@main
+struct KkukApp: App {
+    @UIApplicationDelegateAdaptor private var appDeleagte: AppDelegate
+    
+    var body: some Scene {
+        WindowGroup {}
+    }
+}
