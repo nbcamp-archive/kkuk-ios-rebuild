0e68575bbe7079eabc0cbbc2aba5da0ff8b69ceb
yujinkim1 <yujin7621@me.com>
2025-05-28 02:25:21 +0900

    chore: LinkMetadataEntity 객체와 매핑을 위해 프로퍼티 추가

--- Packages/Domain/Sources/Domain/LinkMetadata.swift
+++ Packages/Domain/Sources/Domain/LinkMetadata.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 public struct LinkMetadata: Sendable {
+    public let id: UUID
     public let ogTitle: String
-    public let ogDescription: String
+    public let ogDescription: String?
     public let ogImage: String?
     public let createdAt: Date
     
-    public init(ogTitle: String, ogDescription: String, ogImage: String?, createdAt: Date) {
+    public init(id: UUID, ogTitle: String, ogDescription: String? = nil, ogImage: String? = nil, createdAt: Date) {
+        self.id = id
         self.ogTitle = ogTitle
         self.ogDescription = ogDescription
         self.ogImage = ogImage
