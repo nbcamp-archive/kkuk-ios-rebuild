0b4c139da4449c67c9605fe6811266dc2583f68c
yujinkim1 <yujin7621@me.com>
2025-05-28 01:11:42 +0900

    feat: SwiftData 모델 코드 추가

---
+++ Packages/Data/Sources/Data/LinkMetadata.swift
@@ -0,0 +1,26 @@
+//
+//  LinkMetadata.swift
+//  Data
+//
+//  Created by Yujin Kim on 2025-05-28.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+public final class LinkMetadata {
+    @Attribute(.unique) public var id: UUID
+    public var ogTitle: String
+    public var ogDescription: String?
+    public var ogImage: String?
+    public var createdAt: Date
+    
+    public init(id: UUID, ogTitle: String, ogDescription: String? = nil, ogImage: String? = nil, createdAt: Date) {
+        self.id = id
+        self.ogTitle = ogTitle
+        self.ogDescription = ogDescription
+        self.ogImage = ogImage
+        self.createdAt = createdAt
+    }
+}
