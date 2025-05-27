a2e713d44cebab29fdd39d1443195c78f276858c
yujinkim1 <yujin7621@me.com>
2025-05-28 01:57:42 +0900

    chore: LinkMetadataRepository.swift 파일 생성

---
+++ Packages/Data/Sources/Data/LinkMetadataRepository.swift
@@ -0,0 +1,15 @@
+//
+//  LinkMetadataRepository.swift
+//  Data
+//
+//  Created by Yujin Kim on 2025-05-28.
+//
+
+import Foundation
+import SwiftData
+
+// MARK: - 추상화
+public protocol LinkMetadataRepository {}
+
+// MARK: - 구체화
+public actor LinkMetadataRepositoryImpl: LinkMetadataRepository {}
