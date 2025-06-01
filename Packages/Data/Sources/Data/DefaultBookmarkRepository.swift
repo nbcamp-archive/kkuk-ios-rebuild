2e1eb9af260f688308d7e26c64ec300b05c422cf
yujinkim1 <yujin7621@me.com>
2025-06-01 10:25:10 +0900

    feat: SwiftSoup 의존성 테스트를 위한 DefaultBookmarkRepository 임시 코드 작성

--- Packages/Data/Sources/Data/DefaultBookmarkRepository.swift
+++ Packages/Data/Sources/Data/DefaultBookmarkRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftData
+import SwiftSoup
 import Domain
 
 final class DefaultBookmarkRepository: BookmarkRepository {
@@ -15,6 +16,20 @@ final class DefaultBookmarkRepository: BookmarkRepository {
     
     public init(modelContext: ModelContext) {
         self.modelContext = modelContext
+    }
+    
+    func parseMetadata(from url: URL) async throws -> Domain.Bookmark {
+        let document = try SwiftSoup.parse(url.absoluteString)
+        let url = try document.select("meta[property=og:url]").attr("content")
+        let title = try document.select("meta[property=og:title]").attr("content")
+        let description = try document.select("meta[property=og:description]").attr("content")
+        let image = try document.select("meta[property=og:image]").attr("content")
+        
+        return Bookmark(
+            ogUrl: url,
+            ogTitle: title,
+            ogDescription: description,
+            ogImage: image)
     }
     
     func fetch() async throws -> [Domain.Bookmark] {
@@ -29,5 +44,35 @@ final class DefaultBookmarkRepository: BookmarkRepository {
         }
         
         return []
+    }
+    
+    // id 충돌만 일어나지 않으면 중복으로 추가해도 됨
+    // 따로 update하거나 save할 필요 없이 단순하게 저장만 하도록 구현해야함
+    func save(_ bookmark: Bookmark) async throws {
+        // Bookmark 모델을 SwiftData 전용 모델로 매핑
+        let entity = BookmarkEntity.toEntity(bookmark)
+
+        do {
+            modelContext.insert(entity) // 데이터 추가
+            try modelContext.save() // 저장
+        } catch {
+            print("\(#function)에러: \(error.localizedDescription)")
+        }
+    }
+    
+    // 선택한 id의 데이터를 삭제함
+    func delete(_ bookmark: Bookmark) async throws {
+        // FetchDescriptor의 Predicate 매크로를 사용해 id로 매칭되는 데이터를 찾음
+        let fetchDescriptor = FetchDescriptor<BookmarkEntity>(
+            predicate: #Predicate{ $0.id == bookmark.id }
+        )
+        
+        do {
+            let result = try modelContext.fetch(fetchDescriptor)
+            result.forEach { modelContext.delete($0) } // 데이터 삭제
+            try modelContext.save() // 저장
+        } catch {
+            print("\(#function)에러: \(error.localizedDescription)")
+        }
     }
 }
