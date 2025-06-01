b4fdcb7eb86bff5420396e26d5b63ab01d43ae17
yujinkim1 <yujin7621@me.com>
2025-06-01 10:25:10 +0900

    feat: SwiftSoup 의존성 테스트를 위한 DefaultBookmarkRepository 임시 코드 작성

--- Packages/Domain/Sources/Domain/BookmarkRepository.swift
+++ Packages/Domain/Sources/Domain/BookmarkRepository.swift
@@ -10,4 +10,10 @@ import Foundation
 // MARK: - Abstraction
 public protocol BookmarkRepository {
     func fetch() async throws -> [Bookmark]
+    func save(_ bookmark: Bookmark) async throws
+    func delete(_ bookmark: Bookmark) async throws
+    /// 오픈그래프 메타데이터를 파싱해서 Bookmark 모델을 생성
+    /// - 북마크를 저장하기 위한 전처리 과정이라 BookmarkRepository에 포함
+    /// - Parameter url: 메타데이터를 파싱할 URL
+    func parseMetadata(from url: URL) async throws -> Bookmark
 }
