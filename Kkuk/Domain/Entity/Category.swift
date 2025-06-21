38e5ac64df0297dfd0f2c4e9841a6ff9e090ae1a
yujinkim1 <yujinkim1.dev@gmail.com>
2025-06-21 15:19:40 +0900

    chore: Category.swift 파일의 코드 deprecated 처리

--- Kkuk/Domain/Entity/Category.swift
+++ Kkuk/Domain/Entity/Category.swift
@@ -5,18 +5,20 @@
 //  Created by 장가겸 on 10/26/23.
 //
 
-import RealmSwift
-
 import Foundation
 
-class Category: Object, Identifiable {
-    
-    @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var name: String
-    @Persisted var iconId: Int
-
-    override static func primaryKey() -> String? {
-        return "id"
-    }
-    
-}
+// MARK: - DEPRECATED, Category, 2025-06-21
+//
+// DEP-005
+// 북마크에 저장되는 카테고리 객체로 사용되었으나,
+// 더 이상 사용하지 않습니다.
+//
+//class Category: Object, Identifiable {
+//    @Persisted(primaryKey: true) var id: ObjectId
+//    @Persisted var name: String
+//    @Persisted var iconId: Int
+//
+//    override static func primaryKey() -> String? {
+//        return "id"
+//    }
+//}
