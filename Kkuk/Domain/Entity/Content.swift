25eb329aabda01a40bf201af043550171c6f9fb8
yujinkim1 <yujinkim1.dev@gmail.com>
2025-06-21 15:16:01 +0900

    chore: Content.swift 파일의 코드 deprecated 처리

--- Kkuk/Domain/Entity/Content.swift
+++ Kkuk/Domain/Entity/Content.swift
@@ -5,41 +5,43 @@
 //  Created by se-ryeong on 2023/10/27.
 //
 
-import RealmSwift
-
 import Foundation
 
-class Content: Object, Identifiable {
-    
-    @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var sourceURL: String
-    @Persisted var title: String
-    @Persisted var imageURL: String?
-    @Persisted var memo: String?
-    @Persisted var createDate: Date
-    @Persisted var isPinned: Bool
-    @Persisted var category: ObjectId
-    
-    override class func primaryKey() -> String? {
-        "id"
-    }
-    
-    convenience init(
-        sourceURL: String,
-        title: String,
-        imageURL: String?,
-        memo: String?,
-        createDate: Date = Date(),
-        isPinned: Bool = false,
-        category: ObjectId) {
-            self.init()
-            self.sourceURL = sourceURL
-            self.title = title
-            self.imageURL = imageURL
-            self.memo = memo
-            self.createDate = createDate
-            self.isPinned = isPinned
-            self.category = category
-        }
-    
-}
+// MARK: - DEPRECATED, Content, 2025-06-21
+//
+// DEP-004
+// 로컬 데이터베이스에 저장되는 북마크 객체로 사용되었으나,
+// 더 이상 사용하지 않습니다.
+//
+//class Content: Object, Identifiable {
+//    @Persisted(primaryKey: true) var id: ObjectId
+//    @Persisted var sourceURL: String
+//    @Persisted var title: String
+//    @Persisted var imageURL: String?
+//    @Persisted var memo: String?
+//    @Persisted var createDate: Date
+//    @Persisted var isPinned: Bool
+//    @Persisted var category: ObjectId
+//    
+//    override class func primaryKey() -> String? {
+//        "id"
+//    }
+//    
+//    convenience init(
+//        sourceURL: String,
+//        title: String,
+//        imageURL: String?,
+//        memo: String?,
+//        createDate: Date = Date(),
+//        isPinned: Bool = false,
+//        category: ObjectId) {
+//            self.init()
+//            self.sourceURL = sourceURL
+//            self.title = title
+//            self.imageURL = imageURL
+//            self.memo = memo
+//            self.createDate = createDate
+//            self.isPinned = isPinned
+//            self.category = category
+//        }
+//}
